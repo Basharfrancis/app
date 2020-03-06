6adc9a370598190fe9be9376cfb2ed2d329039df
Bashar Francis <bfrancis@nuundigital.com>
2020-03-05 22:26:02 -0700

    Updates

---
+++ precache-manifest.bcccbcc2915a7ee9374b737ee0eee17e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2d48f74139000aea45e9ba5dd54c78",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "2bb990b005e4cf2ed7bc",
+    "url": "/app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f09eb437b30521d2a5e1",
+    "url": "/app/static/css/main.ca3c67cb.chunk.css"
+  },
+  {
+    "revision": "2bb990b005e4cf2ed7bc",
+    "url": "/app/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/app/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09eb437b30521d2a5e1",
+    "url": "/app/static/js/main.00cc6973.chunk.js"
+  },
+  {
+    "revision": "8b2c0ae267b0eaf72c20",
+    "url": "/app/static/js/runtime-main.ee4146e8.js"
+  }
+]);
